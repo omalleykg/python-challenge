5a162b9b804d9a10d7e06c91617acd173547053d
Katherine <omalleykg@gmail.com>
2024-08-06 17:22:00 -0400

    PyBank working and writing to text file

--- PyBank/Resources/main_ko2_v2.py
+++ PyBank/Resources/main_ko2_v2.py
@@ -56,3 +56,23 @@ print("Total:", total_value)
 print("Average Change:", average_change )
 print("Greatest Increase in Profits:", max_month_increase, "($", max_increase, ")" )
 print("Greatest Decrease in Profits:", max_month_decrease, "($", max_decrease, ")" )
+
+# Convert an integer to a string
+total_months = 86
+total_months_str = str(total_months)
+total_value = 22564198
+total_value_str = str(total_value)
+average_change = -8311.105882352942
+average_change_str = str(average_change)
+max_increase_str = str(max_increase)
+max_decrease_str = str(max_decrease)
+
+
+
+# Open a file in write mode ('w')
+with open("output_PyBank.txt", "w") as file:
+    file.write("Total Months: " + total_months_str + "\n")
+    file.write("Total: " + total_value_str + "\n")
+    file.write("Average Change: " + average_change_str + "\n")
+    file.write("Greatest Increase in Profits: " + max_month_increase + " ($" + max_increase_str + ")\n")
+    file.write("Greatest Decrease in Profits: " + max_month_decrease + " ($" + max_decrease_str + ")\n")
